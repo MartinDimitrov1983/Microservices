bdbcf95c0b7511c4cb5e35c01a46568f0c82399d
MartinDimitrov1983 <demonnz@abv.bg>
2023-06-13 14:27:20 +0300

    fix

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './natsWrapper';
-
+import { OrderCancelledListener } from './events/listeners/orderCancelledListener';
+import { OrderCreatedListener } from './events/listeners/orderCreatedListener';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -34,10 +35,8 @@ const start = async () => {
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
 
-        // new TicketCreatedListener(natsWrapper.client).listen();
-        // new TickedUpdatedListener(natsWrapper.client).listen();
-        // new ExpirationCompleteListener(natsWrapper.client).listen();
-        // new PaymentCreatedListener(natsWrapper.client).listen();
+        new OrderCancelledListener(natsWrapper.client).listen();
+        new OrderCreatedListener(natsWrapper.client).listen();
 
         await mongoose.connect(process.env.MONGO_URI, {});
         console.log('Connecting to MogoDB');
